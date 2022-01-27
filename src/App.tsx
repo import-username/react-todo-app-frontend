268903499f81598aea80b3088571e0c647cd43da
import-username <stermanv2@gmail.com>
2022-01-27 17:24:33 -0500

    added authelement module for protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
+import AuthElement from "./components/AuthElement/AuthElement";
 import NotFoundPage from "./components/NotFoundPage/NotFoundPage";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<div>landing page</div>} />
+                <Route path="/" element={<AuthElement loadScreen defaultElement={<div>landing page</div>}><div>todo list page</div></AuthElement>} />
                 <Route path="*" element={<NotFoundPage/>} />
             </Routes>
         </BrowserRouter>
---
+++ src/components/AuthElement/AuthElement.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from "react";
+import type { ReactElement } from "react";
+import { useAuthContext } from "../../context/AuthContext";
+import { Navigate } from "react-router-dom";
+
+interface Props {
+    children: ReactElement | Array<ReactElement>,
+    defaultElement: ReactElement,
+    redirectPath?: string,
+    loadScreen?: boolean | undefined
+}
+
+/**
+ * This component is for rendering authenticated-only components, or another component when not authenticated.
+ * If redirectPath is provided, will redirect instead of rendering non-auth component.
+ * @param {React.ReactElement} defaultElement Element to render when not authenticated.
+ * @param {string} [redirectPath] Path to redirect to in case of failed authentication. Has priority over defaultElement.
+ * @param {ReactElement | Array<ReactElement>} children Child/children elements to render when authenticated.
+ * @param {boolean} loadScreen Renders a loading screen while waiting for auth request to fulfill.
+ */
+export default function AuthElement({ defaultElement, redirectPath, loadScreen, children }: Props) {
+    const { authenticated }: { authenticated: boolean | "loading" }  = useAuthContext();
+
+    return (
+        <>
+            {
+                authenticated === "loading" ? <AuthLoadScreen loadScreen={loadScreen} /> : authenticated
+                ? children
+                : redirectPath ? <Navigate to={redirectPath} /> : defaultElement
+            }
+        </>
+    );
+}
+
+// TODO - add loading screen
+function AuthLoadScreen({ loadScreen }: { loadScreen: boolean | undefined }) {
+    return (
+        loadScreen ?
+        <div>loading</div> :
+        null
+    );
+}
