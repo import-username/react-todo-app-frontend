4acca929ff3aa0154c20b9be0cf148a88387d390
import-username <stermanv2@gmail.com>
2022-04-24 11:36:07 -0400

    refactoried todo list item design

--- src/components/TodoListPage/TodoListItem/TodoListItem.tsx
+++ src/components/TodoListPage/TodoListItem/TodoListItem.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { MouseEvent, MouseEventHandler } from "react";
+import { FaCheck, FaEllipsisV } from "react-icons/fa";
 import { useLocation, useNavigate } from "react-router-dom";
 import type { TodoList } from "../TodoListPage";
 import styles from "./TodoListItem.module.scss";
@@ -8,20 +9,60 @@ export default function TodoListItem({ todoListItem }: { todoListItem: TodoList 
 
     const location = useLocation();
 
-    const title: string = todoListItem.title.substring(0, 55).trim() + (todoListItem.title.length > 55 ? "..." : "");
+    const title: string = todoListItem.title.substring(0, 55).trim() + (todoListItem.title.length > 20 ? "..." : "");
 
-    const date: string = todoListItem.createdAt.split(" ")[1] + " " + todoListItem.createdAt.split(" ")[2];
+    const date: string = getTodoListDate(todoListItem);
 
     function onItemClicked(): void {
         navigate(`/${todoListItem.id}`);
     }
 
     return (
-        <div className={`${styles["todo-list-item-container"]} ${location.pathname.replace("/", "") === (todoListItem.id + "") ? styles["selected-todo-list"] : ""}`} onClick={onItemClicked}>
-            <span className={styles["todo-list-item-title"]}>
-                { title }
-            </span>
-            <span className={styles["todo-list-item-date"]}>{ date }</span>
+        <div className={`${styles["todo-list-item-container"]}`}>
+            <div className={`${styles["todo-list-item-button"]}`} onClick={onItemClicked}>
+                <div className={`${styles["todo-list-completion-status"]}`}>
+                    <FaCheck />
+                </div>
+                <div className={styles["todo-list-item-data-container"]}>
+                    <span className={styles["todo-list-item-title"]}>
+                        { title }
+                    </span>
+                    <span className={styles["todo-list-item-date"]}>{ date }</span>
+                </div>
+            </div>
+            <button className={styles["todo-list-item-options-btn"]}>
+                <FaEllipsisV />
+            </button>
         </div>
     );
+}
+
+function getTodoListDate(todoList: TodoList): string {
+    let date: string;
+
+    if (isListAYearOrOlder(todoList.createdAt)) {
+        const dateDifference: number = getDateDifference(todoList.createdAt);
+
+        if (dateDifference === 1) {
+            date = "A year ago.";
+        } else {
+            date = `${dateDifference} years ago.`;
+        }
+    } else {
+        date = todoList.createdAt.split(" ")[1] + " " + todoList.createdAt.split(" ")[2];
+    }
+
+    return date;
+}
+
+function getDateDifference(createdAt: string): number {
+    if (!isListAYearOrOlder(createdAt)) {
+        return -1;
+    }
+
+    return (new Date().getFullYear() - new Date(createdAt).getFullYear());
+}
+
+function isListAYearOrOlder(createdAt: string): boolean {
+    return (new Date(createdAt).getFullYear() < new Date().getFullYear());
 }
