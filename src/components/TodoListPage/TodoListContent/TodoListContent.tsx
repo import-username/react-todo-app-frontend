e47ab1c8e3544873be98a89367f450808abcdc45
import-username <stermanv2@gmail.com>
2022-04-25 09:49:16 -0400

    a modal for interfacing with todo lists is now displayed for smaller screens

--- src/components/TodoListPage/TodoListContent/TodoListContent.tsx
+++ src/components/TodoListPage/TodoListContent/TodoListContent.tsx
@@ -1,10 +1,34 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useMediaQuery } from "react-responsive";
+import { useNavigate, useParams } from "react-router-dom";
+import { TodoList } from "../TodoListPage";
 import styles from "./TodoListContent.module.scss";
 import TodoListUnselectedMenu from "./TodoListUnselectedMenu/TodoListUnselectedMenu";
 
 export default function TodoListContent() {
+    const isSmallScreen: boolean = useMediaQuery({
+        query: "(max-width: 700px)"
+    });
+
+    return (
+        <>
+            {
+                isSmallScreen ?
+                <SmallScreenContentModal /> :
+                <RegularScreenContentMenu />
+            }
+        </>
+    );
+}
+
+function RegularScreenContentMenu() {
     const { id } = useParams();
+
+    const [todoList, setTodoList]: [TodoList | null, React.SetStateAction<any>] = useState(null);
+
+    useEffect(() => {
+        // fetch todo list data
+    }, []);
 
     return (
         <div className={styles["todo-list-content-container"]}>
@@ -14,5 +38,37 @@ export default function TodoListContent() {
                 :   <TodoListUnselectedMenu />
             }
         </div>
+    );
+}
+
+function SmallScreenContentModal() {
+    const { id } = useParams();
+
+    const navigate = useNavigate();
+
+    const [todoList, setTodoList]: [TodoList | null, React.SetStateAction<any>] = useState(null);
+
+    useEffect(() => {
+        // fetch todo list data
+    }, []);
+
+    function onModalClick(event: any) {
+        const targetName: string = event.target.className.split(" ")[0];
+
+        if (targetName === "todo-list-modal-container") {
+            navigate("/");
+        }
+    }
+
+    return (
+        <>
+            {
+                id ?
+                <div className={`todo-list-modal-container ${styles["todo-list-content-modal-container"]}`} onClick={onModalClick}>
+                    <span>small screen</span>
+                </div> :
+                null
+            }
+        </>
     );
 }
